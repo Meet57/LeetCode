29bf4c87003c61baa9d9dd30a7b22ed466c4b210
Meet Patel <45942161+Meet57@users.noreply.github.com>
2024-10-29 12:33:56 -0400

    Time: 0 ms (100%), Space: 16.4 MB (95.24%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -12,6 +12,8 @@ class Solution:
         two = 1
         one = 2
 
+        # All steps hal zero che and then when you think of nth step two ways possible one and two, if one step taken then it has 2 way and if 2 step taken then just 1 way, add them and then think of the step below it again 2 possibilities, started with 2 to n becase the 2nd step will be taken in the case as 2 and  1 step ways as in 2 ways
+
         for i in range(2,n):
             all = one + two
             two = one
