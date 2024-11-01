4aae27c7d863a3d93a898d5926c2ae114b66bf52
Meet Patel <45942161+Meet57@users.noreply.github.com>
2024-11-01 14:44:18 -0400

    Time: 4 ms (71.44%), Space: 19.9 MB (9.34%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -12,8 +12,7 @@ class Solution:
             if not node:
                 return 0
             
-            left_depth = depth(node.left)
-            right_depth = depth(node.right)
+            left_depth, right_depth = depth(node.left), depth(node.right)
 
             self.diameter = max(self.diameter, left_depth + right_depth)
 
