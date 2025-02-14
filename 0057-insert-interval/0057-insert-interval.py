752f257b82e8af15edd9311b67bb563d5feea1ac
Meet Patel <45942161+Meet57@users.noreply.github.com>
2025-02-14 18:48:53 -0500

    Time: 0 ms (100%), Space: 19.9 MB (20.3%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -5,14 +5,21 @@ class Solution:
         res = []
 
         for i in range(len(intervals)):
+            # First edge case or case where the new interval is les than the upcoming intervals
+            # It simply adds the interval to the res and returns the further arrar and res array which has older intervals as well
             if newInterval[1] < intervals[i][0]:
                 res.append(newInterval)
                 return res + intervals[i:]
+            
+            # 2nd case where the interval is bigger than the current interval to adding the current interval and thinking further for more intervals
             elif newInterval[0] > intervals[i][1]:
                 res.append(intervals[i])
+
+            # Case where we are making the new Interval Bigger by merging the current interval to the newInterval and then comparing to further
             else:
                 newInterval = [min(newInterval[0],intervals[i][0]),max(newInterval[1],intervals[i][1])]
         
+        # Adding the new interval at the end considering case 2 and 3 where its not added yet
         res.append(newInterval)
 
         return res
