097eb3371795d19bb2fa8c82d0454f270a0d7610
Meet Patel <45942161+Meet57@users.noreply.github.com>
2025-02-17 12:47:17 -0500

    Time: 0 ms (100%), Space: 18.2 MB (22.65%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -0,0 +1,30 @@
+# nums = [12, 13, 14, 15, 16, 17, 18, 1, 2, 3, 4, 5, 6, 7, 8]
+# target = 5
+
+# The list is sorted in two different half
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)-1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            
+            # Here the base story is that the area is sorted but in two different half if you get the middle one and check the left and right side you will get either of the side being sorted so this conditions are for the first loop itself and after that it will be the normal binary search as it will always go in the first if condition
+            # Left ride is sorted
+            if nums[left] <= nums[mid]:
+                # Maybe in the left side
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            # Right is sorted
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            
+        return -1
